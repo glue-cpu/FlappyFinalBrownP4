22d199f815e02ca76fc150b3022a1450fec85181
glue-cpu <729868@student.dusd.net>
2025-12-08 12:01:26 -0800

    Added stuff
    Added bird and  jump  mechanic. Now I just need to add the score mechanic. (Skipped animations)

---
+++ BrownFlappyFinalP4/Assets/Scripts/bird.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    public float jumpForce = 200f;
+    
+    private bool dead = false;
+    private Rigidbody2D rb2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead)
+        {
+            if (Input.GetMouseButtonDown (0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, jumpForce));
+            }
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        dead = true;
+    }
+}
